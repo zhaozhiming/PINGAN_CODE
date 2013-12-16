ce90bb72684383d8a3dfa65e9dbfc7cc60b6a18b
zhaozhiming <kingzzm1982@sina.com>
2013-12-16 22:34:21 +0800

    make project work

---
+++ src/main/java/com/github/pps/MainController.java
@@ -0,0 +1,45 @@
+package com.github.pps;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.json.JSONObject;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class MainController {
+    private static final Log log = LogFactory.getLog(MainController.class);
+
+    @Value("${appKey}")
+    private String appKey;
+
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String index(ModelMap model) throws Exception {
+        log.debug("index start");
+        model.put("message", "hello world");
+        return "index";
+    }
+
+    @RequestMapping(value = "/data", method = RequestMethod.GET)
+    public
+    @ResponseBody
+    String data() throws Exception {
+        JSONObject result = new JSONObject();
+        result.put("message", "hello world");
+        return result.toString();
+    }
+
+    @RequestMapping(value = "/property", method = RequestMethod.GET)
+    public
+    @ResponseBody
+    String property() throws Exception {
+        JSONObject result = new JSONObject();
+        result.put("property", appKey);
+        return result.toString();
+    }
+}
---
+++ src/main/webapp/WEB-INF/pages/index.jsp
@@ -0,0 +1,13 @@
+<!doctype html>
+<%@taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+
+<%@ page contentType="text/html; charset=UTF-8" %>
+<head>
+    <base target="_blank"/>
+    <script type="text/javascript" src="http://cdn.staticfile.org/jquery/1.10.2/jquery.min.js"></script>
+
+</head>
+<body>
+message: <c:out value="${message}"/>
+</body>
+</html>
