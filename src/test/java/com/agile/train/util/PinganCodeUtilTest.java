1f62c5e35361d62ff45c91598fd10bbf495343e5
zhaozhiming <kingzzm1982@sina.com>
2013-12-19 20:07:50 +0800

    refactor test case

--- src/test/java/com/agile/train/util/PinganCodeUtilTest.java
+++ src/test/java/com/agile/train/util/PinganCodeUtilTest.java
@@ -1,7 +1,6 @@
 package com.agile.train.util;
 
 import com.agile.train.dto.SourceFile;
-import org.junit.Ignore;
 import org.junit.Test;
 
 import java.util.List;
@@ -26,27 +25,41 @@ public class PinganCodeUtilTest {
         assertThat(result.size(), is(1));
         assertThat(result.get(0).getPath(), is("net/sf/cglib/beans/BeanCopier.java"));
         assertThat(result.get(0).getVersion(), is("2.2"));
+        assertThat(result.get(0).getJarName(), is("cglib-2.2-sources.jar"));
     }
 
     @Test
-    @Ignore
     public void should_search_file_in_repository_by_keyword_return_more_files_when_when_have_many_jars() throws Exception {
         List<SourceFile> result = PinganCodeUtil.searchFileInRepositoryByKeyword("test-repository", "Log");
         assertThat(result.size(), is(16));
-        assertThat(result.get(0).getPath(), is("org/slf4j/impl/Log4jLoggerAdapter.java"));
-        assertThat(result.get(result.size() - 1).getPath(), is("org/apache/commons/logging/LogSource.java"));
-        assertThat(result.get(1).getVersion(), is("1.7.5"));
-        assertThat(result.get(result.size() - 1).getVersion(), is("1.1.1"));
+        for (SourceFile sourceFile : result) {
+            assertSourceFileWhenExist(sourceFile, "org/slf4j/impl/Log4jLoggerAdapter.java",
+                    "slf4j-log4j12-1.7.5-sources.jar", "1.7.5");
+
+            assertSourceFileWhenExist(sourceFile, "org/slf4j/impl/Log4jLoggerAdapter.java",
+                    "slf4j-log4j12-1.7.5-sources.jar", "1.7.5");
+
+            assertSourceFileWhenExist(sourceFile, "org/apache/commons/logging/LogSource.java",
+                    "commons-logging-1.1.1-sources.jar", "1.1.1");
+        }
     }
 
     @Test
-    public void should_test() throws Exception {
+    public void should_return_method_list_when_given_source_code() throws Exception {
 
         String sourceCode = PinganCodeUtil.readSourceCodeByFileNameInJar(
                 "test-repository/cglib-2.2-sources.jar", "net/sf/cglib/beans/BeanCopier.java");
         List<String> result = PinganCodeUtil.findMethodsBySourceCode(sourceCode);
         assertThat(result.size(), is(11));
         assertThat(result.contains("void setSource(Class source)"), is(true));
+    }
+
+    private void assertSourceFileWhenExist(SourceFile sourceFile, String exceptPath,
+                                           String exceptJarName, String exceptVersion) {
+        if (sourceFile.getPath().equals(exceptPath)) {
+            assertThat(sourceFile.getJarName(), is(exceptJarName));
+            assertThat(sourceFile.getVersion(), is(exceptVersion));
+        }
     }
 
 }
