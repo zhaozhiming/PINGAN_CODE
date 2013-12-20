e28a39639f5c854a6ca5c7a77a308ab5937ac05c
zhaozhiming <kingzzm1982@sina.com>
2013-12-20 21:41:48 +0800

    refactor code

--- src/main/java/com/agile/train/util/PinganCodeUtil.java
+++ src/main/java/com/agile/train/util/PinganCodeUtil.java
@@ -18,6 +18,14 @@ import java.util.zip.ZipInputStream;
 
 public class PinganCodeUtil {
 
+    private static final String REGEX_FIND_VERSION = "(\\d(\\.\\d)+)";
+    private static final String REGEX_FIND_METHODS = "(\\w+\\s+\\w+\\s*\\(((\\s*\\w+\\s*(\\[\\])*\\s*\\s+(\\[\\])*\\s*\\w+\\s*(\\[\\])*,?)+)?\\)\\s*(?=\\{))";
+    private static final String IMPLEMENTATION_VERSION = "Implementation-Version";
+    private static final String BUNDLE_VERSION = "Bundle-Version";
+    private static final String SUFFIX_JAVA = ".java";
+    private static final String UNKNOWN_VERSION = "";
+    private static final int VERSION_INDEX = 1;
+
     public static String readSourceCodeByFileNameInJar(String jarFileName, String fileName) throws IOException {
         ZipInputStream jarFileInputStream = null;
         try {
@@ -31,7 +39,8 @@ public class PinganCodeUtil {
         }
     }
 
-    private static String readSourceFileInJar(String fileName, ZipInputStream jarFileInputStream) throws IOException {
+    private static String readSourceFileInJar(String fileName,
+                                              ZipInputStream jarFileInputStream) throws IOException {
         ZipEntry entry;
         while ((entry = jarFileInputStream.getNextEntry()) != null) {
             if (entry.getName().equals(fileName)) {
@@ -53,7 +62,8 @@ public class PinganCodeUtil {
         return result.toString();
     }
 
-    public static List<SourceFile> searchFileInRepositoryByKeyword(String repositoryPath, String searchKeyword) throws IOException {
+    public static List<SourceFile> searchFileInRepositoryByKeyword(String repositoryPath,
+                                                                   String searchKeyword) throws IOException {
         File repository = new File(repositoryPath);
         File[] files = repository.listFiles();
 
@@ -69,6 +79,18 @@ public class PinganCodeUtil {
         return result;
     }
 
+    public static List<String> findMethodsBySourceCode(String srcCode) {
+        Pattern pattern = Pattern.compile(REGEX_FIND_METHODS);
+        Matcher matcher = pattern.matcher(srcCode);
+
+        List<String> methods = Lists.newArrayList();
+        while (matcher.find()) {
+            String methodName = matcher.group(0).trim();
+            methods.add(methodName);
+        }
+        return methods;
+    }
+
     private static List<SourceFile> searchFileInJarByKeyword(File jarFile, String searchKeyword) throws IOException {
         ZipInputStream jarFileInputStream = null;
         try {
@@ -77,7 +99,7 @@ public class PinganCodeUtil {
             ZipEntry entry;
             while ((entry = jarFileInputStream.getNextEntry()) != null) {
                 String path = entry.getName();
-                if (path.endsWith(".java") && path.contains(searchKeyword)) {
+                if (path.endsWith(SUFFIX_JAVA) && path.contains(searchKeyword)) {
                     result.add(new SourceFile(jarFile.getName(), retrieveVersionInJar(jarFile), path));
                 }
             }
@@ -88,52 +110,40 @@ public class PinganCodeUtil {
         }
     }
 
-    public static String retrieveVersionInJar(File jarFile) throws IOException {
+    private static String retrieveVersionInJar(File jarFile) throws IOException {
         JarFile jar = null;
         try {
             jar = new JarFile(jarFile);
             Manifest manifest = jar.getManifest();
 
-            String version = "";
+            String version = UNKNOWN_VERSION;
             Attributes attributes = manifest.getMainAttributes();
-            if (attributes == null) return "";
+            if (attributes == null) return UNKNOWN_VERSION;
 
             for (Object attribute : attributes.keySet()) {
                 Attributes.Name key = (Attributes.Name) attribute;
                 String keyword = key.toString();
-                if (keyword.equals("Implementation-Version") || keyword.equals("Bundle-Version")) {
+                if (keyword.equals(IMPLEMENTATION_VERSION) || keyword.equals(BUNDLE_VERSION)) {
                     version = (String) attributes.get(key);
                     break;
                 }
             }
             jar.close();
 
-            return Strings.isNullOrEmpty(version) ? retrieveVersionInJarName(jarFile.getName()) : version;
+            version = Strings.isNullOrEmpty(version) ? retrieveVersionInJarName(jarFile.getName()) : version;
+            return Strings.isNullOrEmpty(version) ? UNKNOWN_VERSION : version;
         } finally {
             if (jar != null) jar.close();
         }
     }
 
     private static String retrieveVersionInJarName(String jarName) {
-        String regEx = "(\\d(\\.\\d)+)";
+        String regEx = REGEX_FIND_VERSION;
         Pattern pattern = Pattern.compile(regEx);
         Matcher matcher = pattern.matcher(jarName);
         matcher.find();
 
-        return matcher.group(1);
-    }
-
-    public static List<String> findMethodsBySourceCode(String srcCode) {
-        Pattern p = Pattern
-                .compile("(\\w+\\s+\\w+\\s*\\(((\\s*\\w+\\s*(\\[\\])*\\s*\\s+(\\[\\])*\\s*\\w+\\s*(\\[\\])*,?)+)?\\)\\s*(?=\\{))");
-        Matcher m = p.matcher(srcCode);
-
-        List<String> methods = Lists.newArrayList();
-        while (m.find()) {
-            String methodName = m.group(0).trim();
-            methods.add(methodName);
-        }
-        return methods;
+        return matcher.group(VERSION_INDEX);
     }
 
 }
